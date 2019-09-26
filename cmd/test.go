25957846bee1372a8279b7b455b95f1852262a04
Eugene Dementiev <eugene@springload.co.nz>
2019-09-27 11:09:25 +1200

    Correctly describe test command. Remove some commented stuff

--- cmd/test.go
+++ cmd/test.go
@@ -10,13 +10,12 @@ import (
 
 var testCmd = &cobra.Command{
 	Use:   "test",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Tests the profiles",
+	Long: `aws-ssh test tests the AWS profiles found in
+~/.aws/config and ~/.aws/credentials (unless -p option is provided)
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Allows to identify permission issues early.
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		summaries, err := lib.TraverseProfiles(viper.GetStringSlice("profiles"))
 		if err != nil {
